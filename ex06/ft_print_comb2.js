ce8f339374941bc67099164011ff345e4ecb0278
Margot Herail <margot.herail@hotmail.fr>
2023-02-09 13:47:38 +0100

    exo 06 test

--- ex06/ft_print_comb2.js
+++ ex06/ft_print_comb2.js
@@ -1,14 +1,14 @@
-function displayCombinations() {
+function showCombinations() {
     for (let i = 0; i <= 9; i++) {
       for (let j = 0; j <= 9; j++) {
         for (let k = 0; k <= 9; k++) {
           for (let l = 0; l <= 9; l++) {
-            console.log(`${i}${j}${k}${l}`.padStart(4, '0'));
+            console.log(`${i}${j} ${k}${l}`.padStart(5, '0'));
           }
         }
       }
     }
   }
   
-  displayCombinations();
+  showCombinations();
   
